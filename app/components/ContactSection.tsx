74dbcc28bae830374fc0c0cb34e616ed05dbe528
Sana Khuram <sana.khuram.baig@gmail.com>
2025-11-20 10:38:35 +0100

    fixed font

--- app/components/ContactSection.tsx
+++ app/components/ContactSection.tsx
@@ -42,67 +42,64 @@ export default function ContactSection() {
           />
         </div>
 
-        {/* Contact info */}
-        <div className="md:w-1/2 flex flex-col items-start justify-center border-b md:border-b-0 md:border-r border-white p-4 sm:p-6">
-          <h2
-            className={`${display.className ?? display.variable}
-    text-4xl sm:text-5xl md:text-6xl lg:text-7xl
-    text-white font-extrabold mb-4 tracking-tight leading-[1.05]`}
-          >
-            CONTACT
-            <br />
-            ME
-          </h2>
+{/* Contact info */}
+<div className="md:w-1/2 flex flex-col items-start justify-center border-b md:border-b-0 md:border-r border-white p-4 sm:p-6">
+  <h2
+    className={`${display.className ?? display.variable}
+      text-4xl sm:text-5xl md:text-6xl lg:text-7xl
+      text-white font-extrabold mb-4 tracking-tight leading-[1.05]`}
+  >
+    CONTACT
+    <br />
+    ME
+  </h2>
 
-          <p
-            className={`${body.className ?? body.variable} mb-3 text-white text-base sm:text-lg`}
-          >
-            I’m open for web development projects, collaborations, and UX/UI design work.
-            Feel free to reach out!
-          </p>
-          <p
-            className={`${body.className ?? body.variable} mb-2 text-white text-base sm:text-lg`}
-          >
-            Email: <strong>sana.khuram.baig@gmail.com</strong>
-          </p>
-          <p
-            className={`${body.className ?? body.variable} text-white text-base sm:text-lg`}
-          >
-            Phone: <strong>+47 40140362</strong>
-          </p>
-        </div>
+  <p className={`${body.className ?? body.variable} mb-3 text-white text-base sm:text-lg`}>
+    I’m open for web development projects, collaborations, and UX/UI design work.
+    Feel free to reach out!
+  </p>
+
+  <p className={`${body.className ?? body.variable} mb-2 text-white text-base sm:text-lg`}>
+    Email: <strong>sana.khuram.baig@gmail.com</strong>
+  </p>
+
+  <p className={`${body.className ?? body.variable} text-white text-base sm:text-lg`}>
+    Phone: <strong>+47 40140362</strong>
+  </p>
+</div>
+
 
         {/* Form */}
         <div className="md:w-1/2 flex flex-col gap-2 sm:gap-3 p-4 sm:p-6">
           <form onSubmit={handleSubmit} className="flex flex-col gap-2">
             <label
-              className={`${body.className ?? body.variable} flex flex-col text-sm sm:text-base text-white font-semibold`}
+              className={`${body.className ?? body.variable} flex flex-col text-sm sm:text-base text-white`}
             >
               Name: *
               <input
                 type="text"
                 name="name"
                 placeholder="Your Name"
-                className="mt-1 px-2 py-1 rounded-md text-white placeholder-white border border-white/50 bg-transparent w-70 focus:outline-none focus:border-dashed focus:border-white"
+                className="mt-1 px-2 py-1 rounded-md text-red-900 placeholder-white border border-white/50 bg-transparent w-70 focus:outline-none focus:border-dashed focus:border-white"
                 required
               />
             </label>
 
             <label
-              className={`${body.className ?? body.variable} flex flex-col text-sm sm:text-base text-white font-semibold`}
+              className={`${body.className ?? body.variable} flex flex-col text-sm sm:text-base text-white `}
             >
               Email: *
               <input
                 type="email"
                 name="email"
                 placeholder="Your Email"
-                className="mt-1 px-2 py-1 rounded-md text-white placeholder-white border border-white/50 bg-transparent w-70 focus:outline-none focus:border-dashed focus:border-white"
+                className="mt-1 px-2 py-1 rounded-md text-red-900 placeholder-white border border-white/50 bg-transparent w-70 focus:outline-none focus:border-dashed focus:border-white"
                 required
               />
             </label>
 
             <label
-              className={`${body.className ?? body.variable} flex flex-col text-sm sm:text-base text-white font-semibold`}
+              className={`${body.className ?? body.variable} flex flex-col text-sm sm:text-base text-white `}
             >
               Message: * (max 1000 characters)
               <textarea
